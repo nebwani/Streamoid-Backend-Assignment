c24cdf58252055979833b18049f95000c7412254
Lakshya Nebwani <lakshya.nebhwani@gmail.com>
2025-10-15 19:10:11 +0530

    build search products feature in product controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,6 +2,7 @@ import ProductSchema from "../models/product.model";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js"
+import { Op } from "sequelize";
 
 
 // List Products
@@ -16,3 +17,24 @@ const listProducts = asyncHandler(async(req, res) => {
         .status(200)
         .json(new ApiResponse(200, products, "Products fetched successfully"));
 })
+
+// search products
+
+const searchProducts = asyncHandler(async(req, res) => {
+    const {brand, color, minPrice, maxPrice} = req.query;
+
+    const where = {};
+
+    if(brand) where.brand = brand;
+    if(color) where.color = color;
+    if(minPrice || maxPrice){
+        if(minPrice) where.price[Op.gte] = parseFloat(minPrice);
+        if(maxPrice) where.price[Op.lte] = parseFloat(maxPrice);
+    }
+
+    const products = await ProductSchema.findAll({where});
+
+    return res.status(200).json(new ApiResponse(200, products, "Filtered products fetched successfully!"));
+})
+
+export {listProducts, searchProducts};
